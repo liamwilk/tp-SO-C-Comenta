806bcee4b45ed0b38576f5f12b5fedd1e1ec3f8a
Liam <liamewilk@gmail.com>
2024-04-07 18:20:47 +0000

    feat: interface of conexiones

---
+++ utils/src/utils/conexiones.c
@@ -0,0 +1,20 @@
+#include <utils/conexiones.c>
+
+int iniciar_servidor(t_log* logger, const char* name, char* ip, char* puerto){
+    //TODO
+    return 0;
+}
+
+int esperar_cliente(t_log* logger, const char* name, int socket_servidor){
+    //TODO
+    return 0;
+}
+
+int crear_conexion(t_log* logger, const char* server_name, char* ip, char* puerto){
+    //TODO
+    return 0;
+}
+
+void liberar_conexion(int* socket_cliente){
+    //TODO
+}
---
+++ utils/src/utils/conexiones.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_CONEXIONES_H_
+#define UTILS_CONEXIONES_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* @fn    decir_hola
+* @brief Imprime un saludo al nombre que se pase por parámetro por consola.
+*/
+int iniciar_servidor(t_log* logger, const char* name, char* ip, char* puerto);
+
+int esperar_cliente(t_log* logger, const char* name, int socket_servidor),
+
+int crear_conexion(t_log* logger, const char* server_name, char* ip, char* puerto);
+
+void liberar_conexion(int* socket_cliente);
+
+#endif
