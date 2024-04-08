487a9f5bcd766ac76c5203697db7e7be634b101f
cardo <jcardosolandin@frba.utn.edu.ar>
2024-04-08 18:42:17 +0000

    Funcion iniciar_servidor, y includes en conexiones.h

--- utils/src/utils/conexiones.c
+++ utils/src/utils/conexiones.c
@@ -1,8 +1,34 @@
-#include <utils/conexiones.c>
+#include "conexiones.h"
 
 int iniciar_servidor(t_log* logger, const char* name, char* ip, char* puerto){
-    //TODO
-    return 0;
+
+    int socketServidor;
+
+    struct addrinfo hints, *serverinfo;
+
+    // Inicializar hints
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    getaddrinfo(ip, puerto, &hints, &serverinfo);
+
+    // Crear socket de escucha del servidor
+    socketServidor = socket(serverinfo->ai_family,
+                            serverinfo->ai_socktype,
+                            serverinfo->ai_protocol);
+    
+    // Asociar socket a un puerto
+    bind(socketServidor, serverinfo->ai_addr, serverinfo->ai_addrlen);
+
+    // Escuchar conexiones entrantes
+    listen(socketServidor, SOMAXCONN);
+
+    freeaddrinfo(serverinfo);
+    log_trace(logger, "%s escuchando en %s:%s \n", name, ip, puerto);
+
+    return socketServidor;
 }
 
 int esperar_cliente(t_log* logger, const char* name, int socket_servidor){
--- utils/src/utils/conexiones.h
+++ utils/src/utils/conexiones.h
@@ -3,14 +3,18 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <sys/socket.h>
+#include <commons/log.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
 /**
 * @fn    decir_hola
 * @brief Imprime un saludo al nombre que se pase por parámetro por consola.
 */
 int iniciar_servidor(t_log* logger, const char* name, char* ip, char* puerto);
 
-int esperar_cliente(t_log* logger, const char* name, int socket_servidor),
+int esperar_cliente(t_log* logger, const char* name, int socket_servidor);
 
 int crear_conexion(t_log* logger, const char* server_name, char* ip, char* puerto);
 
