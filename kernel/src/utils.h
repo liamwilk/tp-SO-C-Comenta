fef6838c0b73b4e8d6d2d74a89cb7b45f80447f3
kenzogrosvald <grokengit config --global user.email>
2024-04-08 01:09:01 +0000

    Primera version de conexion de modulos, metodología secuencial (Memoria, Cpu, Kernel, I/O). Hay que agregar hilos

---
+++ kernel/src/utils.h
@@ -0,0 +1,69 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<readline/readline.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<limits.h>
+//#include <utils/hello.h>
+
+typedef enum
+{
+	MENSAJE,
+	PAQUETE
+}op_code;
+
+typedef struct
+{
+	int size;
+	void* stream;
+} t_buffer;
+
+typedef struct
+{
+	op_code codigo_operacion;
+	t_buffer* buffer;
+} t_paquete;
+
+typedef struct 
+{
+    t_log* logger;
+	char* ip;
+    int puerto;
+	int socket;
+} t_paqueteCliente;
+
+typedef struct 
+{
+    t_log* logger;
+    int puerto;
+} t_paqueteServidor;
+
+t_log* iniciar_logger(char*);
+t_config* iniciar_config(t_log* logger);
+void terminar_programa(int, t_log*, t_config*);
+int crear_conexion(t_log*,char*, int);
+void* serializar_paquete(t_paquete*, int);
+void enviar_mensaje(char*, int);
+void crear_buffer(t_paquete*);
+void agregar_a_paquete(t_paquete*, void*, int);
+void enviar_paquete(t_paquete*, int);
+void eliminar_paquete(t_paquete*);
+void liberar_conexion(int);
+
+int iniciar_servidor(t_log*, int);
+int esperar_cliente(t_log*, int);
+void comunicarConCliente(t_paqueteCliente*);
+int recibir_operacion(int);
+void recibir_mensaje(t_log*,int);
+void *recibir_buffer(int*,int);
+void* threadServidor(void*);
+void* threadMemoria(void*);
+#endif /* MAIN_H_ */
