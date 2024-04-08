791d4e648e12690fb8aef9bfbf8481035541ab03
Liam <liamewilk@gmail.com>
2024-04-08 18:50:44 +0000

    feat of crear_conexion function && fix of conexiones.h

--- utils/src/utils/conexiones.c
+++ utils/src/utils/conexiones.c
@@ -1,6 +1,6 @@
 #include "conexiones.h"
 
-int iniciar_servidor(t_log* logger, const char* name, char* ip, char* puerto){
+{
 
     int socketServidor;
 
@@ -18,7 +18,7 @@ int iniciar_servidor(t_log* logger, const char* name, char* ip, char* puerto){
     socketServidor = socket(serverinfo->ai_family,
                             serverinfo->ai_socktype,
                             serverinfo->ai_protocol);
-    
+
     // Asociar socket a un puerto
     bind(socketServidor, serverinfo->ai_addr, serverinfo->ai_addrlen);
 
@@ -31,16 +31,52 @@ int iniciar_servidor(t_log* logger, const char* name, char* ip, char* puerto){
     return socketServidor;
 }
 
-int esperar_cliente(t_log* logger, const char* name, int socket_servidor){
-    //TODO
+int esperar_cliente(t_log *logger, const char *name, int socket_servidor)
+{
+    // TODO
     return 0;
 }
 
-int crear_conexion(t_log* logger, const char* server_name, char* ip, char* puerto){
-    //TODO
-    return 0;
+int crear_conexion(t_log *logger, const char *server_name, char *ip, char *puerto)
+{
+    struct addrinfo hints;
+    struct addrinfo *server_info;
+
+    // Se inicializa hints
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    // Recibe addrinfo
+    getaddrinfo(ip, puerto, &hints, &server_info);
+
+    // Crea un socket con la informacion recibida (del primero, suficiente)
+    int socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+
+    // Caso en el que falle la creación del socket
+    if (socket_cliente == -1)
+    {
+        log_error(logger, "Error creando el socket para %s:%s", ip, puerto);
+        return 0;
+    }
+
+    // Caso en el que falle la conexión
+    if (connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1)
+    {
+        log_error(logger, "Error al conectar (a %s)\n", server_name);
+        freeaddrinfo(server_info);
+        return 0;
+    }
+    else
+        log_info(logger, "Cliente conectado en %s:%s (a %s)\n", ip, puerto, server_name);
+
+    freeaddrinfo(server_info); // Se libera memoria
+
+    return socket_cliente;
 }
 
-void liberar_conexion(int* socket_cliente){
-    //TODO
+void liberar_conexion(int *socket_cliente)
+{
+    // TODO
 }
--- utils/src/utils/conexiones.h
+++ utils/src/utils/conexiones.h
@@ -2,22 +2,16 @@
 #define UTILS_CONEXIONES_H_
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <commons/log.h>
+#include <signal.h>
 #include <unistd.h>
+#include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
-/**
-* @fn    decir_hola
-* @brief Imprime un saludo al nombre que se pase por parámetro por consola.
-*/
-int iniciar_servidor(t_log* logger, const char* name, char* ip, char* puerto);
+#include <commons/log.h>
 
-int esperar_cliente(t_log* logger, const char* name, int socket_servidor);
-
-int crear_conexion(t_log* logger, const char* server_name, char* ip, char* puerto);
-
-void liberar_conexion(int* socket_cliente);
+int iniciar_servidor(t_log *logger, const char *name, char *ip, char *puerto);
+int esperar_cliente(t_log *logger, const char *name, int socket_servidor);
+int crear_conexion(t_log *logger, const char *server_name, char *ip, char *puerto);
+void liberar_conexion(int *socket_cliente);
 
 #endif
