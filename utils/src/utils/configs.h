56762cb82929a5d06c8397e47d1e4a6db4f44fe2
alexFiorenza <alexfiorenza2012@gmail.com>
2024-04-12 02:16:08 +0000

    Utils: Se agregan las funciones de hilos y handshakes

--- utils/src/utils/configs.h
+++ utils/src/utils/configs.h
@@ -1,3 +1,5 @@
+#ifndef CONFIGS_H_
+#define CONFIGS_H_
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,16 +7,18 @@
 #include <commons/config.h>
 #include <unistd.h>
 #include <limits.h>
+
 /**
  * @fn    iniciar_config
  * @brief Inicializacion de la config de module.config
  * @param logger Instancia de t_log
  */
 t_config *iniciar_config(t_log *logger);
-
 /**
  * @fn    iniciar_logger
  * @brief Inicializacion del logger para los debugs
  * @param nombreDelModulo Es el nombre que le vas a dar al modulo que queres loguear
  */
 t_log *iniciar_logger(char *nombreDelModulo);
+
+#endif
