04d55649757ac78c0cff4857ce5891935507196a
Alex Fiorenza <alexfiorenza2012@gmail.com>
2024-05-02 14:29:17 +0000

    Fix: Funcion handshake actualizada

--- entradasalida/src/generic.c
+++ entradasalida/src/generic.c
@@ -1,24 +1,29 @@
 #include "generic.h"
 
-void* procesar_entradasalida_gen(t_entradasalida entradasalida, t_log *logger, char* nombre_modulo)
+void *procesar_entradasalida_gen(t_entradasalida entradasalida, t_log *logger, char *nombre_modulo)
 {
     // La interfaz generica tiene realmente un unico hilo de ejecucion.
     // No seria necesario crear otro thread para atender la conexion con el kernel.
 
-    int socket_kernel = crear_conexion(logger,entradasalida.ipKernel,entradasalida.puertoKernel);
-	handshake(logger,socket_kernel,1,"Kernel");
-    
+    int socket_kernel = crear_conexion(logger, entradasalida.ipKernel, entradasalida.puertoKernel);
+    t_handshake resultado = esperar_handshake(logger, socket_kernel, ENTRADA_SALIDA_KERNEL, "Kernel");
+    if (resultado != CORRECTO)
+    {
+        liberar_conexion(socket_kernel);
+        pthread_exit(0);
+    }
     identificar_modulo(nombre_modulo, socket_kernel);
 
-	log_info(logger,"Conectado a Kernel en socket %d",socket_kernel);
+    log_info(logger, "Conectado a Kernel en socket %d", socket_kernel);
 
-    while(1)
+    while (1)
     {
-		log_info(logger,"Esperando paquete de Kernel en socket %d",socket_kernel);
-		t_paquete* paquete = recibir_paquete(logger, socket_kernel);
+        log_info(logger, "Esperando paquete de Kernel en socket %d", socket_kernel);
+        t_paquete *paquete = recibir_paquete(logger, socket_kernel);
 
         // Rompo el while cuando se cae la conexion
-        if(paquete->codigo_operacion < 0 || paquete->codigo_operacion > IO_IDENTIFICADOR){
+        if (paquete->codigo_operacion < 0 || paquete->codigo_operacion > IO_IDENTIFICADOR)
+        {
             liberar_conexion(socket_kernel);
             log_info(logger, "Ejecucion de interfaz generica terminada.");
             break;
@@ -29,7 +34,7 @@ void* procesar_entradasalida_gen(t_entradasalida entradasalida, t_log *logger, c
         case IO_GEN_SLEEP:
         {
             log_info(logger, "%u : <%u> - Operacion a realizar: IO_GEN_SLEEP", process_getpid(), process_getpid());
-            
+
             uint32_t unidades_de_trabajo = deserializar_unidades_de_trabajo(paquete);
             log_debug(logger, "Unidades: %d", unidades_de_trabajo);
             usleep(unidades_de_trabajo * entradasalida.tiempoUnidadDeTrabajo);
@@ -38,7 +43,7 @@ void* procesar_entradasalida_gen(t_entradasalida entradasalida, t_log *logger, c
             t_paquete *aviso_sleep = crear_paquete(IO_GEN_SLEEP_TERMINADO);
             // Con el paquete vacio no me anda, si le agrego el uint32_t si...
             actualizar_buffer(aviso_sleep, sizeof(uint32_t));
-            serializar_uint32_t(32,aviso_sleep);
+            serializar_uint32_t(32, aviso_sleep);
             enviar_paquete(aviso_sleep, socket_kernel);
             log_debug(logger, "Paquete enviado");
             eliminar_paquete(aviso_sleep);
@@ -46,7 +51,7 @@ void* procesar_entradasalida_gen(t_entradasalida entradasalida, t_log *logger, c
         }
         default:
             log_info(logger, "No conozco la operacion recibida (%d)", paquete->codigo_operacion);
-            t_paquete* exit = crear_paquete(IO_AVISO_EXIT);
+            t_paquete *exit = crear_paquete(IO_AVISO_EXIT);
 
             enviar_paquete(exit, socket_kernel);
             eliminar_paquete(exit);
--- entradasalida/src/generic.h
+++ entradasalida/src/generic.h
@@ -1,14 +1,14 @@
 #ifndef GENERIC_H_
 #define GENERIC_H_
 
-#include<commons/log.h>
+#include <commons/log.h>
 #include <commons/process.h>
 #include <utils/conexiones.h>
 #include <utils/handshake.h>
 #include <utils/serial.h>
 #include "common.h"
 
-void* procesar_entradasalida_gen(t_entradasalida entradasalida, t_log *logger, char* nombre_modulo);
+void *procesar_entradasalida_gen(t_entradasalida entradasalida, t_log *logger, char *nombre_modulo);
 uint32_t deserializar_unidades_de_trabajo(t_paquete *paquete);
 
 #endif
