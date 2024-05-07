9458f0cc5bfb56aad059ee2072c0c05161767ddc
kenzogrosvald <groken@frba.utn.edu.ar>
2024-05-07 10:46:15 +0000

    Se soluciona error de semantica en revisar_paquete

--- utils/src/utils/serial.c
+++ utils/src/utils/serial.c
@@ -173,7 +173,7 @@ void revisar_paquete(t_paquete *paquete, t_log *logger, char *modulo)
 	}
 	else
 	{
-		log_info(logger, "Kernel solicito el apagado del modulo de %s", modulo);
+		log_info(logger, "Kernel solicito el apagado del modulo.", modulo);
 	}
 }
 
