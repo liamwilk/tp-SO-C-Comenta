ee6f930a8e12e3b0d3cead91d812de5c2ece3344
Liam <liamewilk@gmail.com>
2024-05-21 18:28:45 +0000

    ahora si, mas descriptivo

--- kernel/src/hilos.c
+++ kernel/src/hilos.c
@@ -236,7 +236,7 @@ void *hilo_planificador(void *args)
             if (hiloArgs->kernel->proceso_termino) // Verifico la flag que avisa que un proceso termino
             {
                 kernel_transicion_exec_exit(hiloArgs->estados);
-                hiloArgs->kernel->proceso_termino = false; // Cambio la flag, no terminó el proceso en ejecución
+                hiloArgs->kernel->proceso_termino = false; // Vuelvo a cambiar a estado inicial la flag
             }
 
             if (!obtener_key_finalizacion_hilo(hiloArgs))
