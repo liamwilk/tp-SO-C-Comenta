6222630bef0c8d7993172c564cfe88cdda65ef66
kenzogrosvald <groken@frba.utn.edu.ar>
2024-06-26 02:50:08 +0000

    Se reemplazan referencias a la configuracion de TLB

--- cpu/src/instruccion_ciclo/mmu.c
+++ cpu/src/instruccion_ciclo/mmu.c
@@ -53,7 +53,7 @@ void *hilo_mmu(void *args_void)
             {
                 // En este punto, ya tengo el numero de frame cargado en TLB, asi que lo busco y lo guardo en la estructura
 
-                if (config_get_int_value(args->config, "CANTIDAD_ENTRADAS_TLB") > 0)
+                if (args->config_leida.cantidadEntradasTlb > 0)
                 {
                     args->marco = buscar_en_tlb(args->proceso.pid, args->pagina, args->config_leida.cantidadEntradasTlb, args->tlb);
                 }
